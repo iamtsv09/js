2f42a905c25bb6c735f22b4adefa9d48669e701d
iamtsv <alesya0910@gmail.com>
2022-08-04 15:22:28 +0300

    task2isdone

--- task2/index.js
+++ task2/index.js
@@ -1,5 +1,6 @@
-for (let the_number = 1; the_number <= 100; the_number++)
-    if (the_number % 3 == 0 && the_number % 5 == 0) console.log('FizzBuzz');
-    else if (the_number % 3 == 0) console.log('Fizz');
-    else if (the_number % 5 == 0) console.log('Buzz');
-    else console.log(the_number);
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz');
+    else if (i % 3 == 0) console.log('Fizz');
+    else if (i % 5 == 0) console.log('Buzz');
+    else console.log(i);
+}
