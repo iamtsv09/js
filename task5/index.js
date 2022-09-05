63855646586872f75c1baccd8032391b8acece05
iamtsv <alesya0910@gmail.com>
2022-09-05 20:50:07 +0300

    task5

--- task5/index.js
+++ task5/index.js
@@ -1,6 +1,6 @@
 function isEven(n) {
-    if (n == 0) return true;
-    if (n == 1) return false;
+    if (n === 0) return true;
+    if (n === 1) return false;
     if (n > 1) return isEven(n - 2);
 }
 console.log(isEven(50));
