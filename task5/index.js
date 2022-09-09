762ab7f70399cff98f797e9eb697d9c25815c8be
iamtsv <alesya0910@gmail.com>
2022-09-09 22:55:50 +0300

    task5

--- task5/index.js
+++ task5/index.js
@@ -1,12 +1,4 @@
 function isEven(n) {
-    if (n === 0) return true;
-    if (n === 1) return false;
-    if (n > 1) return isEven(n - 2);
-}
-console.log(isEven(50));
-console.log(isEven(75));
-
-function isEven(n) {
     if (n === 0) {
         return 'Четное';
     } else if (n === 1) {
